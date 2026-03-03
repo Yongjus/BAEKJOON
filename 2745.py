5f0c846f36947d192d0c6aa57f477755d0b44c03
Juyong Ko <rhwndyd54@gmail.com>
2026-03-03 11:29:38 +0900

    Create 2745.py

---
+++ 2745.py
@@ -0,0 +1,18 @@
+import sys
+
+mapping = {f"{i}": i for i in range(10)}
+for i in range(26):
+    char = chr(ord('A') + i)
+    mapping[char] = 10 + i
+
+N, B = sys.stdin.readline().split()
+B = int(B)
+
+res = 0
+if B > 10:
+    for i in range(len(N)-1, -1, -1):
+        res = res + mapping[N[-i-1]] * (B**i)
+    else:
+        print(res)
+else:
+    print(int(N, B))
