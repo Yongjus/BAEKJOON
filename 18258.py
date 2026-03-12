556c4ec88b875abd93f4df5a0102e141434086d7
Juyong Ko <rhwndyd54@gmail.com>
2026-03-12 13:51:03 +0900

    Create 18258.py

---
+++ 18258.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+queue = deque([])
+N = int(sys.stdin.readline())
+for i in range(N):
+    command = sys.stdin.readline().rstrip()
+    if command.startswith("push"):
+        _, num = command.split()
+        queue.append(num)
+    elif command == "pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif command == "size":
+        print(len(queue))
+    elif command == "empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif command == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif command == "back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
